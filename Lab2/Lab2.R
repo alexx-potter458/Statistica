5c874f141b78245fc9a647b75fff8ad7cce758da
alexx-potter458 <alexx.potter458@gmail.com>
2021-10-14 22:33:37 +0300

    first commit

---
+++ Lab2/Lab2.R
@@ -0,0 +1,44 @@
+#Lab 2 Statisics
+n <- 10
+cat("Ne place aici ", "acum de ", n, "minute")
+objects()
+ls()
+cat("obiectle de aici\nface asa")
+rm()
+m<-2323
+rm(m)
+rm(list=objects)
+?rm
+
+d1 <- 2
+d2 <- 4
+d3 <- 55
+5 -> d4
+rez <- d1 + d3 -> d4 -> d5
+
+v <- c(1, 3, 4)
+
+1:10
+30:3
+
+seq(5,9,0.1)
+rep(1, 2)
+c(2,5)
+rep(c(2,3), c(1,4))
+
+set.seed(15129)
+x <- sample(1:1000,100)
+
+x[x%%8==0][1:3]
+x_1 <-  x[-(1:3)]
+
+
+
+
+
+
+#tema:
+d2+d4 -> d6 <- d1
+  #cu paranteze?
+rez <- d1 + d3 -> d4 -> d5
+
