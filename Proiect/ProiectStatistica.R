edcc8c63491513296bef11c05535f9dc4aa9205c
alexx-potter458 <alexx.potter458@gmail.com>
2022-02-02 13:17:29 +0200

    f

--- Proiect/ProiectStatistica.R
+++ Proiect/ProiectStatistica.R
@@ -1,11 +1,12 @@
 #Olaru Alexandru / Matei Elena / Negru Bogdan / Trache Andrei
 
-# I.)
+# I.) ##########################################################################
+
 ## 1.
 
-a <- 0
-b <- 1
-c <- 1
+a <- 1
+b <- 6
+c <- 20
 iterations <- 10^7
 
 average <- function() { #calculam numarul de elemente aleatorii necesare din (a,b) pentru a depasi c
@@ -27,20 +28,36 @@ average <- function() { #calculam numarul de elemente aleatorii necesare din (a,
     return(Inf)
 }
 
-
 aproxK <- sum(replicate(iterations, average()))/iterations #calculam media 
 
+## 1+. pentru intervalul(0,1)
 
-################################################################################
+average <- function() { #calculam numarul de elemente aleatorii necesare din (a,b) pentru a depasi c
+  x <- 0
+  sum <- 1
+  counter <- 1
+  repeat {
+    x <- 1/factorial(counter)
+    sum <- sum + x
+    if(counter + 1 > iterations) break  
+    counter <- counter + 1
+  }
+  return(sum)
+  
+}
+
+aproxK <- replicate(1, average())
+
 ## 2.
 
 exactK <- ((2*c)/(a+b)) + 1
 errorK <- (abs(exactK - aproxK)/exactK)*100 #calculam eroarea in procente
 
-# II.)
+# II.)##########################################################################
+
 ## 1.
 
-iterations <- 10^7 #numarul de persoane
+iterations <- 10^6 #numarul de persoane
 n <- 20 #numarul de etape
 
 alfa <- runif(n-1, 0, 1) #probabilitatea ca o persoana sa termine fiecare activitate, ca vector
@@ -68,15 +85,15 @@ runStages <- function() { #persoana isi parcurge etapele pana la esec
 totalTimes <- replicate(iterations,runStages()) #timpul T pentru fiecare persoana, ca vector
 averageTime <- sum(totalTimes)/iterations # media timpului necesar fiecarei persoane
 
-################################################################################
+
 ## 3.
 
 finishProb <- prod(alfa) #se inmultesc probabilitatile din alfa
 
-################################################################################
+
 ## 4.
 
-sigma <- runif(1, min(Ti), max(Ti)) #generam un timp aleator
+sigma <- runif(1, min(cumsum(Ti)), max(cumsum(Ti))) #generam un timp aleator
 counter <- 0
 timeSum <- 0
 
@@ -99,43 +116,35 @@ if(sigma >= Ti[1]) { #daca sigma e prea mic atunci nu se termian prima etapa
   
 }
 
-################################################################################
+
 ## 5.
 
 minTime <- min(totalTimes) #timpul minim
 maxTime <- max(totalTimes) #timpul maxim
 hist(totalTimes)
 
-################################################################################
+
 ## 6.
 
 k <- 5
-
 probabilities <- c(1 - alfa[1])
-time <- Ti[1]
 
 for (i in 2:(n-1)) {
-  tempValue <- 1
-  
-  for(j in 1:(i-1)) {
-    tempValue <- tempValue * alfa[j]
-    
-  }
-  
-  tempValue <- tempValue * (1 - alfa[i])
+  tempValue <-  prod(alfa[1:(i-1)])*(1-alfa[i])
   probabilities <- c(probabilities, tempValue)
-  
 }
 
 kProbabilities <- cumprod(probabilities) #vectorul de probabilitati de a ajunge la k
 kProb <- kProbabilities[k] #prbabilitatea de ajunge la etapa k
 
-plot(kProbabilities)
-lines(kProbabilities)
+plot(kProbabilities[1:k])
+lines(kProbabilities[1:k])
 
-# III.)
+
+# III.) ########################################################################
 
 require("ConvergenceConcepts")
+
 
 ## 1.
 
@@ -177,6 +186,5 @@ dataUniPL  <- check.convergence(nmax=2, M = 5000, genXn=valuesGenUnif,  mode="p"
 ## 3.
 
 
-
-# IV.)
+# IV.) #########################################################################
 
